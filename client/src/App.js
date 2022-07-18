d400be8ea08ba179cc919520f0e55aedac4b5a27
Muskaan Agrawal <73891478+ma-user@users.noreply.github.com>
2022-07-18 19:33:49 +0530

    Add files via upload

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          One-Queue Front-End will work here!
+        </p>
+        <a href="http://localhost:8080/">Go to Server</a>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;

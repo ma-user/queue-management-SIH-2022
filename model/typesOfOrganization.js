1964748f0803098c32c994e2c14b9bb186a75240
Muskaan Agrawal <73891478+ma-user@users.noreply.github.com>
2022-09-03 13:02:45 +0530

    Delete model directory

--- model/typesOfOrganization.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const typesOfOrganizationSchema = new mongoose.Schema({
-    // Array of Strings
-    organizationType: {type: Array, default: []},
-});
-
-module.exports = mongoose.model('TypesOfOrganization', typesOfOrganizationSchema);
